45be4277330ada8a6c6ab9744e6ac20d73dd6503
xomya4ok <xomya4ok2@gmail.com>
2022-02-20 17:29:08 +0300

    Добавьте файлы проекта.

---
+++ MyForm.cpp
@@ -0,0 +1,12 @@
+#include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ arg) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    Lab1::MyForm form;
+    Application::Run(% form);
+}
